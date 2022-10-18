abae2f61314d596510dcb4c21b5dbd505e29b88f
Surelyx <speedlouis250@gmail.com>
2022-10-17 22:06:56 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,24 +21,13 @@ int main(void)
 					putchar(l);
 					if (n != 55 || m != 56)
 					{
-						putchar(',');                                       
-                                            
-			                              
-						 *
-						 *  digits                                                                                                                        
-						 *
-						 *   * Return: Always 0 (Success)                                                                                                 
-						 *
-						 *    */                                                                                                                           
-
-						int main(void)                                                                                                                
-
-						{                                                                                                                             
-
-							        int n, m, l;                                                                   								                                                                                                                              
-
-								        for (n = 48; n < 58; n++)                                                                                             
-
-										        {                                                                                                                     
-
-												                for (m = 49; m < 58; m++)                                                  																			          																																																			     
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
